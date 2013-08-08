8bac1c43d95ecf4326544653eca822d59bbaa9b7
Sergey Zakharchuk <sergeyzakharchuk@Zakharchuk-Sergey-Aplear.local>
2013-08-08 19:20:31 +0300

    db connect

---
+++ DB_check.php
@@ -0,0 +1,21 @@
+<?php
+
+
+define('HOST','localhost');
+define('USER','');
+define('PASS','');
+define('DBASE','');
+define('ENCODING','');
+
+class DB_Check{
+
+function __construction(){
+
+        $db = mysql_connect(HOST,USER,PASS) OR DIE('databasse errore!');
+        mysql_select_db(DBASE,$db) OR DIE('wrong databasse');
+        mysql_query('SET NAMES',ENCODING, $db) OR DIE('fuck charset');
+
+}
+
+}
+?>
